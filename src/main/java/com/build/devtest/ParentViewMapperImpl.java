1407ec8b4a806bf02abd70ea4e0b103a0d46cd6f
Jayesh Tambe <tambe_jayesh@hotmail.com>
2017-04-15 04:16:49 -0700

    msg

--- src/main/java/com/build/devtest/ParentViewMapperImpl.java
+++ src/main/java/com/build/devtest/ParentViewMapperImpl.java
@@ -2,31 +2,36 @@
 
 package com.build.devtest;
 
+import java.util.Collections;
 import java.util.List;
 
-public class ParentViewMapperImpl Implements ParentViewMapper
+
+public class ParentViewMapperImpl implements ParentViewMapper
 {
 
 	public List<ParentView> mapRowsToViews(List<ParentRow> parentRows, List<ChildRow> childRows)
 	{			
 
-		List<ParentView> prvwlist = new List<ParentView>();
-		for(ParentRow prrow : parentRows)
+		List<ParentView> prvwlist = Collections.emptyList();
+		for (ParentRow prrow : parentRows)
 		{			
-			List<ChildView> chvwlist = new List<ChildView>();
-			ParentView prvw = new ParentView(prrow.firstName, prrow.lastName, prrow.age, prrow.parentId, chvwlist);		
+			List<ChildView> chvwlist = Collections.emptyList();
+			ParentView prvw = new ParentView(prrow.getFirstName(), 
+				prrow.getLastName(), prrow.getAge(), prrow.getParentId(), chvwlist);		
 			
-			for(ChildRow childrow : childRows)
+			for (ChildRow childrow : childRows)
 			{
-				if(childrow.parentId == prrow.ParentId)
+				if(childrow.getParentId() == prrow.getParentId())
 				{
-					ChildView chvw = new ChildView(childrow.firstName, childrow.lastName, childrow.age, childrow.childId, null);
+					ChildView chvw = new ChildView(childrow.getFirstName(),
+						childrow.getLastName(), childrow.getAge(), childrow.getChildId(), null);
+					
 					chvwlist.add(chvw);
 				}
 				
 			}
 
-			prvwlist.add(prrow);
+			prvwlist.add(prvw);
 		}
 
 		return prvwlist;
