d008385d489875bf2faf96166cf23c3b49b4c911
Jayesh Tambe <tambe_jayesh@hotmail.com>
2017-04-16 14:55:31 -0700

    modified java list to ArrayList as list is interface

--- src/main/java/com/build/devtest/ParentViewMapperImpl.java
+++ src/main/java/com/build/devtest/ParentViewMapperImpl.java
@@ -4,24 +4,24 @@ package com.build.devtest;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.ArrayList;
 
 
 public class ParentViewMapperImpl implements ParentViewMapper
 {
-
 	public List<ParentView> mapRowsToViews(List<ParentRow> parentRows, List<ChildRow> childRows)
 	{			
 
-		List<ParentView> prvwlist = Collections.emptyList();
+		ArrayList<ParentView> prvwlist = new ArrayList<ParentView>();
 		for (ParentRow prrow : parentRows)
 		{			
-			List<ChildView> chvwlist = Collections.emptyList();
+			ArrayList<ChildView> chvwlist = new ArrayList<ChildView>();
 			ParentView prvw = new ParentView(prrow.getFirstName(), 
 				prrow.getLastName(), prrow.getAge(), prrow.getParentId(), chvwlist);		
 			
 			for (ChildRow childrow : childRows)
 			{
-				if(childrow.getParentId() == prrow.getParentId())
+				if(childrow.getParentId().equalsIgnoreCase(prrow.getParentId()) )
 				{
 					ChildView chvw = new ChildView(childrow.getFirstName(),
 						childrow.getLastName(), childrow.getAge(), childrow.getChildId(), null);
@@ -36,5 +36,34 @@ public class ParentViewMapperImpl implements ParentViewMapper
 
 		return prvwlist;
 	}
+
+
+	// public List<ParentView> mapRowsToViews(List<ParentRow> parentRows, List<ChildRow> childRows)
+	// {			
+
+	// 	List<ParentView> prvwlist = Collections.<ParentView>emptyList();
+	// 	for (ParentRow prrow : parentRows)
+	// 	{			
+	// 		List<ChildView> chvwlist = Collections.<ChildView>emptyList();
+	// 		ParentView prvw = new ParentView(prrow.getFirstName(), 
+	// 			prrow.getLastName(), prrow.getAge(), prrow.getParentId(), chvwlist);		
+			
+	// 		for (ChildRow childrow : childRows)
+	// 		{
+	// 			if(childrow.getParentId() == prrow.getParentId())
+	// 			{
+	// 				ChildView chvw = new ChildView(childrow.getFirstName(),
+	// 					childrow.getLastName(), childrow.getAge(), childrow.getChildId(), null);
+					
+	// 				chvwlist.add(chvw);
+	// 			}
+				
+	// 		}
+
+	// 		prvwlist.add(prvw);
+	// 	}
+
+	// 	return prvwlist;
+	// }
 
 }
